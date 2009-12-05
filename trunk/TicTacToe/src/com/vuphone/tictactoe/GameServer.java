948e9255a790a3c53b62445869df695b90db56dd
adammalbright <adammalbright@f402f4c0-dfbb-11de-8dcd-9b777bee35ae>
2009-12-05 17:22:10 +0000

    - Added networking components - Added Lobby activity to allow for starting single or multiplayer games - Added a Server to allow hosting games and responding to game requests - Updated Board to hold turn and connection information

---
+++ trunk/TicTacToe/src/com/vuphone/tictactoe/GameServer.java
@@ -0,0 +1,281 @@
+package com.vuphone.tictactoe;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.BindException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.util.Log;
+
+import com.vuphone.tictactoe.model.Board;
+
+/**
+ * 
+ * @author Adam Albright, Ben Gotow
+ * 
+ */
+public class GameServer extends Thread {
+
+	private static GameServer instance_ = null;
+
+	public static final int RESPONSE_NONE = 3;
+	public static final int RESPONSE_DENY = 2;
+	public static final int RESPONSE_ACCEPT = 1;
+	public static final int RESPONSE_GAME_IN_PROGRESS = 4;
+
+	private boolean SHUTDOWN = false;
+	private ServerSocket s_ = null;
+
+	private String listening_ip_ = null;
+	private int PORT = 1234;
+
+	private final String cmdGameRequest = "<cmd>REQUEST-NEW-GAME</cmd>";
+	private final String cmdAcceptGame = "<cmd>ACCEPT-GAME-REQUEST</cmd>";
+	private final String cmdDenyGame = "<cmd>DENY-GAME-REQUEST</cmd>";
+	private final String cmdGameInProgress = "<cmd>GAME-IN-PROGRESS</cmd>";
+
+	public GameServer() {
+		super("GameServer");
+	}
+
+	@Override
+	public void run() {
+		Log.d("mad", "[*] Listening for game requests...");
+
+		try {
+			s_ = new ServerSocket(PORT);
+			s_.setSoTimeout(2000);
+		} catch (BindException e) {
+			Log.d("mad", " %%%Port is already in use%%%");
+			return;
+		} catch (Exception e) {
+		}
+
+		// lets find our IP address
+		try {
+			Socket socket = new Socket("www.google.com", 80);
+			socket.setSoTimeout(1500);
+			listening_ip_ = socket.getLocalAddress().toString().substring(1);
+			socket.close();
+		} catch (Exception e) {
+			try {
+				listening_ip_ = InetAddress.getLocalHost().getHostAddress();
+			} catch (UnknownHostException e1) {
+				listening_ip_ = "No internet connection";
+			}
+		}
+
+		Log.d("mad", "IP Addr: " + listening_ip_);
+
+		/*
+		 * Accept TCP socket connections
+		 */
+		while (!SHUTDOWN) {
+			try {
+
+				final Socket sock = s_.accept();
+
+				// Spit out next data
+				Log.d("mad", "Incoming game request from "
+						+ sock.getRemoteSocketAddress());
+
+				if (Board.getInstance().isGameInProgress()) {
+					sock.getOutputStream().write(cmdGameInProgress.getBytes());
+				} else {
+
+					LobbyActivity.uiThreadCallback.post(new Runnable() {
+						public void run() {
+							LobbyActivity.getInstance().incomingGameRequestCB(
+									sock);
+						}
+					});
+				}
+			} catch (Exception e) {
+				// We want accept to throw exceptions after timeout to keep from
+				// blocking indefinitely
+			}
+		}
+
+		Log.d("mad", "[*] Data server shutting down!");
+
+		try {
+			s_.close();
+			s_ = null;
+		} catch (IOException e) {
+		}
+	}
+
+	public void die() {
+		SHUTDOWN = true;
+	}
+
+	public void sendGameRequest(final String remoteAddr, final int remotePort) {
+
+		// spawn a new thread to send game request
+		// use guiHandler to callback about response
+
+		new Thread(new Runnable() {
+			public void run() {
+				final Socket sock = sendRequest(remoteAddr, remotePort);
+				LobbyActivity.uiThreadCallback.post(new Runnable() {
+					public void run() {
+						LobbyActivity.getInstance().deliveredRequestCB(
+								(sock == null) ? false : true);
+
+						if (sock == null)
+							return;
+
+						new Thread(new Runnable() {
+
+							public void run() {
+								final int result = waitOnGameRequest(sock);
+								LobbyActivity.uiThreadCallback
+										.post(new Runnable() {
+											public void run() {
+												LobbyActivity.getInstance()
+														.requestResponseCB(
+																result, sock);
+											}
+										});
+							}
+						}).start();
+					}
+				});
+			}
+		}).start();
+	}
+
+	private Socket sendRequest(String remoteAddr, int remotePort) {
+		// write the object
+		Socket sock = getSocket(remoteAddr, remotePort);
+		if (sock == null)
+			return null;
+
+		try {
+			OutputStream out = sock.getOutputStream();
+			out.write(cmdGameRequest.getBytes());
+			return sock;
+		} catch (Exception e) {
+			System.err.println("Error! Connection to opponent lost!");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void responseToRequest(Socket sock, boolean iaccept) {
+		try {
+			if (iaccept)
+				sock.getOutputStream().write(cmdAcceptGame.getBytes());
+			else {
+				sock.getOutputStream().write(cmdDenyGame.getBytes());
+				sock.close();
+			}
+		} catch (IOException e) {
+			System.err.println("Error! Connection to opponent lost!");
+			e.printStackTrace();
+		}
+	}
+
+	private int waitOnGameRequest(Socket sock) {
+		String s = readCmdFromSocket(sock, 8);
+		if (s != null)
+			Log.d("mad", "Got a packet! " + s);
+
+		if (s == null)
+			return RESPONSE_NONE;
+		else if (s.equals(cmdAcceptGame))
+			return RESPONSE_ACCEPT;
+		else if (s.equals(cmdDenyGame))
+			return RESPONSE_DENY;
+		else if (s.equals(cmdGameInProgress))
+			return RESPONSE_GAME_IN_PROGRESS;
+		else
+			return RESPONSE_NONE;
+	}
+
+	private String readCmdFromSocket(Socket sock, int timeoutSEC) {
+		InputStream in;
+
+		try {
+			sock.setSoTimeout(2000);
+			in = sock.getInputStream();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return null;
+		}
+
+		StringBuilder xml = new StringBuilder();
+		Pattern regex = Pattern.compile("(<cmd>.+</cmd>)", Pattern.DOTALL);
+		Matcher regexMatcher = null;
+		int c = 0;
+
+		// Waits until a packet is received between START & END flags
+		while (in != null && sock.isConnected()) {
+			try {
+				c = in.read();
+				if (c == -1) {
+					in.close();
+					in = null;
+				}
+			} catch (Exception e) {
+				// Exception is thrown once a second
+				--timeoutSEC;
+				if (timeoutSEC == 0)
+					return null;
+				else
+					continue;
+			}
+
+			xml.append((char) c);
+
+			if (((char) c) == '>') {
+				regexMatcher = regex.matcher(xml);
+				if (regexMatcher.find())
+					return regexMatcher.group(1);
+			}
+		}
+
+		return null;
+	}
+
+	private Socket getSocket(String remoteAddr, int remotePort) {
+		Socket sock = null;
+		try {
+			sock = new Socket(remoteAddr, remotePort);
+
+			if (!sock.isConnected())
+				return null;
+		} catch (Exception e) {
+			return null;
+		}
+
+		return sock;
+	}
+
+	public static GameServer getInstance() {
+		if (instance_ == null) {
+			instance_ = new GameServer();
+			instance_.start();
+		}
+
+		return instance_;
+	}
+
+	public String getMyIP() {
+		int count = 10;
+		while ((--count) > 0 && listening_ip_ == null) {
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+			}
+		}
+
+		return listening_ip_;
+	}
+}
---
+++ trunk/TicTacToe/src/com/vuphone/tictactoe/LobbyActivity.java
@@ -0,0 +1,163 @@
+package com.vuphone.tictactoe;
+
+import java.net.Socket;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.vuphone.tictactoe.model.Board;
+
+public class LobbyActivity extends Activity implements OnClickListener {
+
+	private static Context context_ = null;
+	private static LobbyActivity instance_ = null;
+	public static Button btnStart_ = null;
+	static final Handler uiThreadCallback = new Handler();
+
+	/**
+	 * Called when the activity is first created and again after the apps goes
+	 * offscreen and resumes
+	 */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		context_ = getBaseContext();
+		instance_ = this;
+
+		btnStart_ = (Button) findViewById(R.id.btnSendRequest);
+		btnStart_.setOnClickListener(this);
+		((Button) findViewById(R.id.btnSinglePlayer)).setOnClickListener(this);
+
+		// Get IP addr
+		String ip = GameServer.getInstance().getMyIP();
+		((TextView) findViewById(R.id.lblMyIP)).setText("My IP: " + ip);
+	}
+
+	/**
+	 * Start or SinglePlayer button is clicked
+	 */
+	public void onClick(View v) {
+
+		String btn = ((Button) v).getText().toString();
+
+		if (btn.equals("Single Player Mode")) {
+			Board.getInstance().startNewGame();
+			Intent i = new Intent(this, GameActivity.class);
+			startActivity(i);
+			return;
+		}
+
+		btnStart_.setClickable(false);
+
+		TextView ip = (TextView) findViewById(R.id.server);
+		TextView port = (TextView) findViewById(R.id.port);
+
+		try {
+			String addy = ip.getText().toString();
+			int p = Integer.parseInt(port.getText().toString());
+			GameServer.getInstance().sendGameRequest(addy, p);
+		} catch (Exception e) {
+			echo("Invalid opponent information!");
+		}
+
+	}
+
+	public void deliveredRequestCB(boolean success) {
+		if (success) {
+			echo("Delivered request! Awaiting reply...");
+		} else {
+			echo("Unable to connect to the remote player");
+			btnStart_.setClickable(true);
+		}
+	}
+
+	public void requestResponseCB(int response, Socket sock) {
+		try {
+			if (response == GameServer.RESPONSE_ACCEPT) {
+				echo("Opponent has accepted your challenge! Starting game...");
+
+				Board.getInstance().setOpponentSocket(sock);
+				Board.getInstance().startNewGame();
+
+				Intent i = new Intent(this, GameActivity.class);
+				startActivity(i);
+				return;
+
+			} else if (response == GameServer.RESPONSE_DENY) {
+				echo("Opponent has denied your challenge! You win for now.");
+
+			} else if (response == GameServer.RESPONSE_GAME_IN_PROGRESS) {
+				echo("Opponent is currently in a game. Try again later.");
+
+			} else {
+				echo("Opponent didn't response to your request.");
+			}
+
+			btnStart_.setClickable(true);
+			sock.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void incomingGameRequestCB(final Socket sock) {
+		String ip = sock.getRemoteSocketAddress().toString();
+		ip = ip.substring(0, ip.indexOf('/'));
+
+		String msg = "You've got an incoming request from " + ip
+				+ ". Want to play?";
+
+		final Intent act = new Intent(this, GameActivity.class);
+		AlertDialog dialog = new AlertDialog.Builder(LobbyActivity.this)
+				.create();
+		dialog.setMessage(msg);
+		dialog.setButton(DialogInterface.BUTTON_POSITIVE, "Yes",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int i) {
+						dialog.cancel();
+						GameServer.getInstance().responseToRequest(sock, true);
+						Board.getInstance().setOpponentSocket(sock);
+						Board.getInstance().startNewGame();
+
+						startActivity(act);
+						return;
+					}
+				});
+
+		dialog.setButton(DialogInterface.BUTTON_NEGATIVE, "No",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int i) {
+						dialog.cancel();
+						GameServer.getInstance().responseToRequest(sock, false);
+					}
+				});
+
+		dialog.show();
+	}
+
+	public static LobbyActivity getInstance() {
+		if (instance_ == null)
+			System.err.println("Error! LobbyAct->getInst before onCreate");
+
+		return instance_;
+	}
+
+	public static void echo(String msg) {
+		Toast.makeText(context_, msg, Toast.LENGTH_SHORT).show();
+	}
+
+}
